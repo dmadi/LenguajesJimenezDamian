286835c320cd7d573b26ca80a66f53214245f593
T107 <T107@PC253>
2014-01-20 19:14:01 -0600

    Primera Practica

---
+++ LenguajesDam/src/lenguajesdam/LenguajesDam.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lenguajesdam;
+
+/**
+ *
+ * @author T107
+ */
+public class LenguajesDam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenido al curso de Lenguajes");
+    }
+    
+}
